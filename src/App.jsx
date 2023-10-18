42792451d4bdba7c9ab0b6652841af9b174e25d2
Eka Priyanthara <ekapriyanthara072@gmail.com>
2023-10-18 18:50:27 +0800

    fix responsibility for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const colors = [
 
 function App() {
   const [quote, setQuote] = useState([]);
-  const [color, setColor] = useState("#222");
+  const [color, setColor] = useState("#222222");
 
   const fetchQuotes = async () => {
     const response = await fetch(
@@ -39,24 +39,24 @@ function App() {
 
   return (
     <div
-      className="flex justify-center items-center h-screen transition-colors duration-500"
+      className="flex gap-2 justify-center items-center h-screen transition-colors duration-500 select-none"
       style={{ backgroundColor: color }}
     >
-      <div className="flex flex-col gap-7 bg-white p-10 rounded-lg w-[576px] mx-10">
+      <div className="flex flex-col gap-7 bg-white p-10 rounded-lg w-[450px] mx-10">
         <div className="flex flex-col gap-7 font-lato" style={{ color: color }}>
-          <p className="flex justify-center text-center text-2xl">
-            <span className="me-2">
-              <FaQuoteLeft size={27} />
+          <p className="text-center text-lg md:text-xl lg:text-2xl clear-both font-medium">
+            <span className="flex justify-center mb-5 md:me-3 md:mb-0 md:inline-block">
+              <FaQuoteLeft size={27} className="md:before:content-['\f10d']" />
             </span>
             {quote.length === 0 ? "Unknown" : quote.quote}
           </p>
-          <p className="flex justify-end text-xl">
+          <p className="flex justify-end text-base md:text-lg lg:text-xl">
             {quote.length === 0 ? "— Unknown" : "— " + quote.author}
           </p>
         </div>
-        <div className="flex justify-end">
+        <div className="flex justify-center md:justify-end">
           <button
-            className="py-2 px-5 text-white rounded-md font-poppins text-base hover:opacity-90 ease-in-out transition-all duration-300"
+            className="py-2 px-5 text-white rounded-md font-poppins text-xs md:text-sm lg:text-base hover:opacity-90 ease-in-out transition-all duration-300"
             style={{ backgroundColor: color }}
             onClick={onClickHandler}
           >
